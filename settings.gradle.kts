38969c57b16d1650ca0f6a662dc95db925af6fb6
zamanazimi100-bit <324774947+zamanazimi100-bit@users.noreply.github.com>
2026-09-18 14:11:30 +0430

    Fix Android JVM target compatibility

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,26 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+plugins {
+    id("com.android.application") version "8.7.3"
+    id("org.jetbrains.kotlin.android") version "2.0.21"
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
+android {
+    namespace = "com.azimi.guardian"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.azimi.guardian"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
-
-rootProject.name = "AZIMI Guardian"
-include(":app")
