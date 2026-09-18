d6643ca8d87c06c75b8e7ed106170e610ecebcb1
zamanazimi100-bit <324774947+zamanazimi100-bit@users.noreply.github.com>
2026-09-18 14:31:57 +0430

    UpFix Guardian Gradle repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,26 +1,18 @@
-plugins {
-    id("com.android.application") version "8.7.3"
-    id("org.jetbrains.kotlin.android") version "2.0.21"
-}
-
-android {
-    namespace = "com.azimi.guardian"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.azimi.guardian"
-        minSdk = 29
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "azimi-studio"
+include(":app")
