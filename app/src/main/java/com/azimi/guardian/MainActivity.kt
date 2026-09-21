9bfd822eb2bb5037c1f6c1c8a33d5fc8c79498fa
zamanazimi100-bit <324774947+zamanazimi100-bit@users.noreply.github.com>
2026-09-21 12:41:03 +0430

    Add import for Build class in MainActivity

--- app/src/main/java/com/azimi/guardian/MainActivity.kt
+++ app/src/main/java/com/azimi/guardian/MainActivity.kt
@@ -4,6 +4,7 @@ package com.azimi.guardian
 import android.app.Activity
 import android.content.Intent
 import android.os.BatteryManager
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.os.StatFs
