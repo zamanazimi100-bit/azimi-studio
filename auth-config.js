855a7ece2244130ecc66b9e9934e0a38126bb85f
zamanazimi100-bit <324774947+zamanazimi100-bit@users.noreply.github.com>
2026-09-25 11:54:00 +0430

    Delete auth-config.js

--- auth-config.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    supabasePublishableKey:
-      process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
-  });
-}
